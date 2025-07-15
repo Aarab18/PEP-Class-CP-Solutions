f1c0828992a6350610c74e9f0f20c61539c9bff9
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 22:02:41 +0530

    Third solved on 02-07-2025

---
+++ 02-07-2025/Third/third.java
@@ -0,0 +1,37 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class third {
+    public static TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) return null;
+        if (root.val < low) return trimBST(root.right, low, high);
+        if (root.val > high) return trimBST(root.left, low, high);
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
+    public static void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(4);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(1);
+
+        int low = 1, high = 3;
+        TreeNode trimmed = trimBST(root, low, high);
+        inorder(trimmed);
+    }
+}
