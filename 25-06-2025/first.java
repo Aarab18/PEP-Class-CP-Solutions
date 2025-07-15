1e016a1a479af36061cc590ab6a3f20272b166db
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 18:10:00 +0530

    Added untracker files

---
+++ 25-06-2025/first.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class first {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(findKthLargest(nums, k));
+    }
+}
