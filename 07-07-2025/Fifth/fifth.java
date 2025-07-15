08e7edc1885e40647f3954c0f858ba389f65cde3
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 22:53:26 +0530

    Fifth solved on 07-07-2025

---
+++ 07-07-2025/Fifth/fifth.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class fifth {
+    public static int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(lengthOfLIS(nums));
+    }
+}
