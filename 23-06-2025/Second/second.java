1dca0c73be117b0a633add07a8d3a49ea2cbdf3f
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 17:49:26 +0530

    Second Code Solved on 23-06-2025

---
+++ 23-06-2025/Second/second.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class second {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0], currSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(maxSubArray(nums));
+    }
+}
