520df167318d15703afc8e335953263420f73e75
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 18:00:03 +0530

    Fifth Code Solved on 23-06-2025

---
+++ 23-06-2025/Fifth/fifth.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class fifth {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] result = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] res = productExceptSelf(nums);
+        for (int val : res) {
+            System.out.print(val + " ");
+        }
+    }
+}
