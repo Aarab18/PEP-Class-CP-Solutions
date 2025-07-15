1e543706352dc75ae7d733aaa2fa579e8c22ea68
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 22:19:59 +0530

    Fifth solved on 03-07-2025

---
+++ 03-07-2025/Fifth/fifth.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] dfs(TreeNode node) {
+        if (node == null) return new int[2];
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+        int rob = node.val + left[1] + right[1];
+        int notRob = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return new int[]{rob, notRob};
+    }
+}
+
+public class fifth {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(3);
+        root.right.right = new TreeNode(1);
+
+        Solution sol = new Solution();
+        System.out.println(sol.rob(root));
+    }
+}
