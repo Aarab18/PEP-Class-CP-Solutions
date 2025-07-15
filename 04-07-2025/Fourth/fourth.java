78b2d40cf193728089dde5a7e6f492a455b2e8bf
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 22:36:12 +0530

    Fourth solved on 04-07-2025

---
+++ 04-07-2025/Fourth/fourth.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class fourth {
+    public static int nthUglyNumber(int n) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int i2 = 0, i3 = 0, i5 = 0;
+        for (int i = 1; i < n; i++) {
+            int next = Math.min(ugly[i2] * 2, Math.min(ugly[i3] * 3, ugly[i5] * 5));
+            ugly[i] = next;
+            if (next == ugly[i2] * 2) i2++;
+            if (next == ugly[i3] * 3) i3++;
+            if (next == ugly[i5] * 5) i5++;
+        }
+        return ugly[n - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(nthUglyNumber(n));
+    }
+}
