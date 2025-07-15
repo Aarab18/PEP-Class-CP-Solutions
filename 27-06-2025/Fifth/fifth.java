86b8343c7c64f934c1ac7c92c021ba1fbf7f91c7
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 18:50:18 +0530

    Fifth Solved on 27-06-2025

---
+++ 27-06-2025/Fifth/fifth.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class fifth {
+    public static boolean canJump(int[] nums) {
+        int reach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > reach) return false;
+            reach = Math.max(reach, i + nums[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
+        System.out.println(canJump(nums));
+    }
+}
