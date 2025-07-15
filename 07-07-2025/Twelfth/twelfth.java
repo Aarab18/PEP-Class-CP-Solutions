9806ca6ee18a649df0b852d96b3c893f67ba35ad
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 23:10:55 +0530

    Twelfth solved on 07-07-2025

---
+++ 07-07-2025/Twelfth/twelfth.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class twelfth {
+    public static int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
+    }
+
+    private static int robLinear(int[] nums, int start, int end) {
+        int prev1 = 0, prev2 = 0;
+        for (int i = start; i <= end; i++) {
+            int take = nums[i] + prev2;
+            int skip = prev1;
+            int curr = Math.max(take, skip);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(rob(nums));
+    }
+}
