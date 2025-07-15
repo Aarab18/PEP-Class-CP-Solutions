b53fd4bd85a7e9b079bb192b39fa978c380780d5
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 23:50:43 +0530

    Second solved on 15-07-2025

---
+++ 15-07-2025/Second/second.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class second {
+    public static int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        boolean[] visited = new boolean[n];
+        int[] minDist = new int[n];
+        Arrays.fill(minDist, Integer.MAX_VALUE);
+        minDist[0] = 0;
+        int cost = 0;
+
+        for (int i = 0; i < n; i++) {
+            int u = -1;
+            for (int j = 0; j < n; j++) {
+                if (!visited[j] && (u == -1 || minDist[j] < minDist[u])) {
+                    u = j;
+                }
+            }
+            visited[u] = true;
+            cost += minDist[u];
+
+            for (int v = 0; v < n; v++) {
+                if (!visited[v]) {
+                    int dist = Math.abs(points[u][0] - points[v][0]) + Math.abs(points[u][1] - points[v][1]);
+                    minDist[v] = Math.min(minDist[v], dist);
+                }
+            }
+        }
+
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] points = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            points[i][0] = sc.nextInt();
+            points[i][1] = sc.nextInt();
+        }
+        System.out.println(minCostConnectPoints(points));
+    }
+}
