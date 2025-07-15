ba56eac6c45d94a887b3d90285cd566fae77124d
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 23:07:48 +0530

    Eleventh solved on 07-07-2025

---
+++ 07-07-2025/Eleventh/eleventh.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class eleventh {
+    public static int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = dp[i + 1][j] + 1;
+                for (int k = i + 1; k <= j; k++) {
+                    if (s.charAt(i) == s.charAt(k)) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i + 1][k - 1] + dp[k][j]);
+                    }
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(strangePrinter(s));
+    }
+}
