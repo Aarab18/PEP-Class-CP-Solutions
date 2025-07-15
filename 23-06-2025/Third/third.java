2f669d8bfe391f9e9d8883d5877cf16e11921ea3
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-07-15 17:53:33 +0530

    Third Code Solved on 23-06-2025

---
+++ 23-06-2025/Third/third.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class third {
+    public static int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+        System.out.println(maxArea(height));
+    }
+}
